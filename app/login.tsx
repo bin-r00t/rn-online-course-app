cb9aaa3566e53d221c01e1335bb34eff9f524f70
liubin <bh0bama@sina.com>
2025-04-30 10:47:25 +0800

    feat: upload Login

--- app/login.tsx
+++ app/login.tsx
@@ -1,12 +1,74 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Pressable, TouchableOpacity, ImageBackground } from "react-native";
 
 
 export default function Login() {
   return (
     <View
-      className="flex-1 justify-center items-center bg-gray-100"
+      className="flex-1"
     >
-      <Text className="">Login Page.</Text>
+      <ImageBackground
+        source={require('../assets/images/bg.png')}
+        className="w-full h-full flex-col justify-center px-8"
+        resizeMode="cover"
+      >
+        <View className="flex-col gap-8">
+          <Text className="text-4xl font-bold">Lorem ipsum dolor sit.</Text>
+          <Text className="text-lg text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt autem sit deleniti!!</Text>
+        </View>
+
+        <View className="flex-col my-6 pt-4 gap-6">
+          <Text className="text-2xl font-bold">Sign in</Text>
+          <TextInput placeholder="Email" className="w-full border-none bg-white rounded-xl p-5" />
+          <TextInput placeholder="Code" className="w-full border-none bg-white rounded-xl p-5" />
+        </View>
+
+        <View className="flex-col my-6 gap-6">
+          <TouchableOpacity
+            className="w-full bg-blue-500 rounded-md p-4"
+            onPress={() => {
+              console.log('Login');
+            }}
+          >
+            <Text className="text-center text-white text-lg font-bold">Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            className="w-ful rounded-md"
+            onPress={() => {
+              console.log('to sign up');
+            }}
+          >
+            <Text className="text-center text-gray-800 text-lg font-bold">No Account? Join in</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View className="flex-row items-center justify-center my-4 gap-4">
+          <TouchableOpacity
+            className="w-32 bg-gray-400 rounded-md p-4"
+            onPress={() => {
+              console.log('Login');
+            }}
+          >
+            <Text className="text-center text-white text-lg font-bold">G</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            className="w-32 bg-gray-400 rounded-md p-4"
+            onPress={() => {
+              console.log('Login');
+            }}
+          >
+            <Text className="text-center text-white text-lg font-bold">F</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            className="w-32 bg-gray-400 rounded-md p-4"
+            onPress={() => {
+              console.log('Login');
+            }}
+          >
+            <Text className="text-center text-white text-lg font-bold">T</Text>
+          </TouchableOpacity>
+         
+        </View>
+      </ImageBackground>
     </View>
   );
 }
